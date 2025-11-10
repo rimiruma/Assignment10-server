ca529ba8698b098bf0240fd9a149f98f7bcdb323
rimiruma <rimiruma12@gmail.com>
2025-11-10 09:54:36 +0600

    add properties data done

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+
+const uri = "mongodb+srv://homedbUser:Cj7crJZ29Eqsaq7Z@cluster0.2o3am.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('smart server is running')
+})
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const db = client.db('home_db')
+        const PropertiesCollection = db.collection('Properties')
+
+        // Add properties api
+        app.post('/properties', async (req, res) => {
+            const newProperty = req.body;
+            // console.log(newProperty);
+
+            const result = await PropertiesCollection.insertOne(newProperty);
+            res.send(result);
+        });
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`smart server is running on port: ${port}`);
+
+})
